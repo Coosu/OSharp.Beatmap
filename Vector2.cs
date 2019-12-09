6d53e90ecebf1ff2f6c3640fdfee34c512d4f43c
Milky <milkitic@126.com>
2019-12-09 14:53:17 +0800

    sv

---
+++ Vector2.cs
@@ -0,0 +1,14 @@
+﻿namespace OSharp.Beatmap
+{
+    public struct Vector2
+    {
+        public Vector2(float x, float y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
